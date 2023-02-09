9d67cc622a4d9a1a380e5dcb58c9c3b2a247f569
v3vapp <v3v.app@gmail.com>
2023-02-09 23:58:37 +0900

    🎉 kick in

---
+++ main.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import seaborn as sns
+import pandas as pd
+
+# 株価の時系列データの読み込み
+df = pd.read_csv("stock_data.csv")
+
+# フレームごとの処理
+def update(frame):
+    plt.clf()
+    sns.lineplot(x='Date', y='Close', data=df[df['Date'] <= df['Date'].iloc[frame]])
+    plt.xlabel("Date")
+    plt.ylabel("Close Price")
+    plt.title("Stock Price Over Time")
+
+# アニメーションの作成
+ani = animation.FuncAnimation(plt.gcf(), update, frames=len(df), interval=100)
+
+plt.show()
